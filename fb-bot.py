e36944283dc5fd32178f9246939dbe7bcf3dc276
Python Passive Income <pythontester192@gmail.com>
2022-11-27 19:34:51 +0800

    Last-Update-FB-BOT

--- fb-bot.py
+++ fb-bot.py
@@ -7,15 +7,15 @@ from time import sleep
 
 # Set up Facebook groups to post, you must be a member of the group
 groups_links_list = [
-    "https://web.facebook.com/groups/440155481477202"
+    "https://www.facebook.com/groups/440155481477202"
 ]
 
 # Set up text content to post
 message = """I Found the Easiest Way to Make Money on Instagram Using This Free Bot | Make Money Online 2022 !!.
 Link: https://youtu.be/q_PZ5z7H2Hc"""
 
-# Set up paths of images to post
-photo = 'C:/Users/python/Desktop/Facebook-Group-Bot/image.jpg'
+# Set up the path of images to post
+photo = 'C:/Users/python/Desktop/Python/Facebook-Group-Bot/image.jpg'
 
 # Login Facebook
 options = webdriver.ChromeOptions()
@@ -34,10 +34,10 @@ def main():
         time.sleep(3)
         driver.find_element(By.XPATH,'//span[text()="Photo/video"]').click()
         time.sleep(2)
-        photo_element = driver.find_element(By.XPATH,'//input[@type="file" and @accept="image/*,image/heif,image/heic,video/*,video/mp4,video/x-m4v,video/x-matroska,.mkv"]')
+        photo_element = driver.find_element(By.XPATH,'//input[@multiple and @type="file" and @class="x1s85apg"]')
         photo_element.send_keys(photo)
         time.sleep(3)
-        driver.find_element(By.XPATH,'//div[@class="mfn553m3"]/div').send_keys(message)
+        driver.find_element(By.XPATH,'//*[@class="_1mf _1mj"]').send_keys(message)
         time.sleep(2)
         post_button = driver.find_element(By.XPATH,'//div/span/span[text()="Post"]')
         post_button.click()
